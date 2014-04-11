e2cc92b50c0f2efb341ae5f3948a86fd7f08f3ed
elnappo <elnappoo@gmail.com>
2014-04-11 16:24:35 +0200

    add baulicht-cli.py

--- CLI/baulicht-cli.py
+++ CLI/baulicht-cli.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Baulicht command line interface to change mode, add text,"
+                                             " remove text or pause the blinking.",
+                                 epilog="That's how to do it!")
+
+parser.add_argument("-l", "--list", action='store_true', help="List all active texts")
+parser.add_argument("-m", "--mode", choices=["text", "blink"], help="Set mode to text or blink")
+parser.add_argument("-a", "--add", type=str, metavar='<string>', help="Add Text to morse")
+parser.add_argument("-r", "--remove", type=int, metavar='<int>', help="Remove Text")
+parser.add_argument("-b", "--blink", nargs="?", metavar='<list<int>>', help="Set blink interval")
+parser.add_argument("-p", "--pause", choices=["on", "off"], help="Stop morse/blink")
+
+args = parser.parse_args()
