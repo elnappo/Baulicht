36590360615693511992c698388b1055d573e274
elnappo <elnappoo@gmail.com>
2014-06-18 16:39:22 +0200

    improved cli

--- CLI/baulicht-cli.py
+++ CLI/baulicht-cli.py
@@ -14,8 +14,9 @@ parser.add_argument("-m", "--mode", choices=["text", "blink"], help="Set mode to
 parser.add_argument("-s", "--speed", type=int, metavar='<int>', help="length of a dit in ms")
 parser.add_argument("-b", "--blink", nargs="?", metavar='<list<int>>', help="Set blink interval")
 parser.add_argument("-p", "--pause", choices=["on", "off"], help="Stop morse/blink")
-parser.add_argument("--status", action="store_true", help="Stop morse/blink")
-
+parser.add_argument("--status", action="store_true", help="Show current status")
+parser.add_argument("--start", action="store_true", help="Start morse/blink")
+parser.add_argument("--stop", action="store_true", help="Stop morse/blink")
 args = parser.parse_args()
 
 from baulicht_dbus import BaulichtDbus
@@ -26,10 +27,10 @@ if args.status:
     print("Current System Status:")
     paused = baulicht.paused
     mode = baulicht.mode
-    #speed = baulicht.speed
+    speed = baulicht.speed
     print("Paused: %s" % (paused))
     print("Mode: %s" % (mode))
-    # print("Speed: %s" % (speed))
+    print("Speed: %s" % (speed))
 
 if args.list:
     for text in baulicht.list_text():
@@ -39,13 +40,13 @@ if args.add:
     path = baulicht.add_text(args.add)
     print('Added "%s". Path is "%s"' % (args.add, path))
     
-if args.pause == "on":
-    baulicht.paused = True
+if args.pause == "on" or args.stop:
+    baulicht.stop()
     print("Stop morse")
 
-elif args.pause == "off":
-    baulicht.paused = False
-    print("Stop morse")
+elif args.pause == "off" or args.start:
+    baulicht.start()
+    print("Start morse")
     
 if args.speed:
     baulicht.speed = args.speed
