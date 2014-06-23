4163401029515ba49159356fd293b8472e2d1bff
elnappo <elnappoo@gmail.com>
2014-06-23 14:20:11 +0200

    add admin for message

--- Webinterface/main/admin.py
+++ Webinterface/main/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 
 from main.models import Message
 
+
+def to_dbus(modeladmin, request, queryset):
+    for message in queryset:
+        message.send_to_dbus()
+
+to_dbus.short_description = "Mark selected messages as published"
+
+
+def remove_from_dbus(modeladmin, request, queryset):
+    for message in queryset:
+        message.remove_from_dbus()
+
+remove_from_dbus.short_description = "Mark selected messages as published"
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "text", "accepted", "is_active")
+    list_display_links = ("name", "text")
+
+    list_filter = ("name", "email", "accepted", "is_active", "created", )
+
+
 # Register your models here.
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
