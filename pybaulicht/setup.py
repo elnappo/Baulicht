3d3d0280b60c2b3714209ca23ddb2e73aac39d88
elnappo <elnappoo@gmail.com>
2014-06-23 14:18:52 +0200

    make baulicht wrapper a python packege

---
+++ pybaulicht/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='Baulicht',
+    version='0.1',
+    description='dbus wrapper for Baulicht project',
+    install_requires=['dbus-python'],
+    py_modules=['pybaulicht'],
+    url='https://github.com/elnappo/Baulicht/tree/master/pybaulicht',
+    license='MIT',
+    author='elnappo',
+    author_email='elnappoo@gmail.com'
+)
